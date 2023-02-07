59e40398e33e8c333f2fbf012735f2e4b2391989
raissa <kouabenanraissa@yahoo.fr>
2023-02-07 11:11:33 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+    class User{
+    public $fistname;
+  
+    public function __construct($fistname)
+    {
+        $this->fistname = $fistname;
+       
+    }
+
+    public function hello()
+    {
+        echo "hello". $this->fistname;
+        return $this;
+    }
+        public function register()
+    {
+        echo " >> registered";
+        return $this;
+    }
+
+    public function mail()
+    {
+       echo " >> email sent \n";
+        
+    }
+    }
+$user = new User("jane");
+$user->fistname;
+$user->hello()->register()->mail();
+
+?>
